5b534439822a8c9acdbda53c567f6ea7eaabc976
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-05-28 17:46:44 +0530

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from '../components/ServiceSection';
+import ProjectsSection from '../components/ProjectsSection';
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <HeroSection />
       <AboutSection />
       <ServiceSection />
+      <ProjectsSection />
     </div>
   );
 }
