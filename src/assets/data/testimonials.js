0a1688c9db819019ae70a7b475b56f61bbd04569
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-05-30 09:10:12 +0530

    update

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -1,27 +1,27 @@
 const testimonials = [
   {
     id: 1,
-    name: 'Shaif Arfan',
+    name: 'Juan Despi',
     title: 'CEO',
-    org: 'WEB CIFAR',
+    org: 'Deez Nutzz',
     desc:
-      'Ayan is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+      'Sahil is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
-    name: 'Fahim Elahe',
+    name: 'Anusha Ganapathy',
     title: 'Co-founder',
-    org: 'Kreetive',
+    org: 'AG Capital',
     desc:
       'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
   },
   {
     id: 3,
-    name: 'Sayed',
-    title: 'Marketing Head',
-    org: 'Eleven Dawn',
+    name: 'Chris Parungo',
+    title: 'COO',
+    org: 'Fort Knox',
     desc:
-      'Really hard-working person. Deliver the work as promised. Planning to work more with this person.',
+      'Really hard-working person. Delivers the work as promised. Planning to work more with him.',
   },
 ];
 export default testimonials;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection';
 import AboutSection from '../components/AboutSection';
 import ServiceSection from '../components/ServiceSection';
 import ProjectsSection from '../components/ProjectsSection';
+import TestimonialsSection from '../components/TestimonialsSection';
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <AboutSection />
       <ServiceSection />
       <ProjectsSection />
+      <TestimonialsSection />
     </div>
   );
 }
