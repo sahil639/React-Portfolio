01d4d80e29bc5c414afd17027f6772a35dc4c03e
cuminsoda <44092199+sahil639@users.noreply.github.com>
2021-08-14 18:24:22 +0530

    update

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -9,43 +9,37 @@ const projects = [
   {
     id: uuidv4(),
     name: 'U Tracker',
-    desc:
-      'An application to track your all data from one place. I developed the website and the mobile app',
+    desc: 'An application to track your all data from one place. I developed the website and the mobile app',
     img: UTrackerImg,
   },
   {
     id: uuidv4(),
     name: 'Green CTG',
-    desc:
-      'An app to help people to get an overview of how they can make the city beautiful.',
+    desc: 'An app to help people to get an overview of how they can make the city beautiful.',
     img: GreenCtgImg,
   },
   {
     id: uuidv4(),
     name: 'Coin Tracker',
-    desc:
-      'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
+    desc: 'Using this app you can track any e coin. Also you will get a good advise about investment form the professional',
     img: CoinTrackerImg,
   },
   {
     id: uuidv4(),
     name: "Cavin's Portfolio",
-    desc:
-      'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
+    desc: 'A portfolio for Cavin jr. A artist from New york city. The portfolio is made using ReactJs and GatsbyJs.',
     img: CavinImg,
   },
   {
     id: uuidv4(),
     name: 'Tracking Soft',
-    desc:
-      'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
+    desc: 'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
     img: ProjectImg,
   },
   {
     id: uuidv4(),
     name: 'Tracking Soft',
-    desc:
-      'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
+    desc: 'A tracking website that will show the performance of the website. Also you will get some useful advice to improve the performance.',
     img: ProjectImg,
   },
 ];
--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -4,24 +4,21 @@ const testimonials = [
     name: 'Juan Despi',
     title: 'CEO',
     org: 'Deez Nutzz',
-    desc:
-      'Sahil is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
+    desc: 'Sahil is a person of commitment. He is really good at what he is doing. I really like his work. Especially the unique point of view of designing.',
   },
   {
     id: 2,
     name: 'Anusha Ganapathy',
     title: 'Co-founder',
     org: 'AG Capital',
-    desc:
-      'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
+    desc: 'Really amazing communication skills. Always understand what I am trying to achieve. Also, his work is really amazing. Do really high-quality work.',
   },
   {
     id: 3,
     name: 'Chris Parungo',
     title: 'COO',
     org: 'Fort Knox',
-    desc:
-      'Really hard-working person. Delivers the work as promised. Planning to work more with him.',
+    desc: 'Really hard-working person. Delivers the work as promised. Planning to work more with him.',
   },
 ];
 export default testimonials;
